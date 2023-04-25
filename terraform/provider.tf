3e33ab18511f7faba4ba213792bb544b71e0bfdb
Yurii <yuriimeda@gmail.com>
2023-04-25 21:46:01 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region  =   "us-east-1" # обраний регіон
-    access_key  =   "AKIAXTQ42XHPS5AK6HF6" # ключ доступу
-    secret_key  =   "HTXY/l2mDFUfOZxm7lmnNgdP/vCQ4YidA29+2xwk" # секретний ключ
+    access_key  =   "AKIAXTQ42XHPZVBFGWHF" # ключ доступу
+    secret_key  =   "b2PJjKhQIEHXv1yfzNemG2XnSmXlcudwXBLzMncN" # секретний ключ
 }
