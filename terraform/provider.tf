bcc47a9873dfad0c5744478952bf8c578fe57c8b
Yura <67182548+Areshokk@users.noreply.github.com>
2023-04-25 21:46:55 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region  =   "us-east-1" # обраний регіон
-    access_key  =   "AKIAXTQ42XHPZVBFGWHF" # ключ доступу
-    secret_key  =   "b2PJjKhQIEHXv1yfzNemG2XnSmXlcudwXBLzMncN" # секретний ключ
+    access_key  =   "" # ключ доступу
+    secret_key  =   "" # секретний ключ
 }
